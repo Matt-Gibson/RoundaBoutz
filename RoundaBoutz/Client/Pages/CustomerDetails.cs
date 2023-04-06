008d8c877f7db541ca6bc0c0bf64d5ce3bfa5272
Matt Gibson <gibson.matthew@hotmail.com>
2023-04-05 23:40:53 -0400

    In Process of Editing page for Customers

--- RoundaBoutz/Client/Pages/CustomerDetails.cs
+++ RoundaBoutz/Client/Pages/CustomerDetails.cs
@@ -7,7 +7,7 @@ namespace RoundaBoutz.Client.Pages;
 public partial class CustomerDetails 
 {
     protected string Message = string.Empty;
-    protected Customer customer = new Customer();
+    protected Customer customer {get;set;} = new Customer();
     [Parameter]
     public Guid Id {get;set;}
 
@@ -16,5 +16,34 @@ public partial class CustomerDetails
 
     [Inject]
     private NavigationManager navigationManager {get; set;}
+
+    protected async override Task OnInitializedAsync()
+    {
+        if(Id == Guid.Empty)
+        {
+            // Add new Customer
+        }
+
+        else
+        {
+            //Update existing Customer
+            var apiCustomer = await customerService.GetCustomer(Id);
+
+            if(apiCustomer != null)
+            {
+                customer = apiCustomer;
+            }
+        }
+    }
+
+    protected void HandleFailedRequest()
+    {
+        Message = "An error occured, please check your entries and try again.";
+    }
+
+    protected void GoToCustomers()
+    {
+        navigationManager.NavigateTo("/Customers");
+    }
 
 }
